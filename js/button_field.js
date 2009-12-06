d1c84c2c8c16bc86d2abf0b8e90f4e1dd251221a
James I. Armes <bassistjimmyjam@284457.no-reply.drupal.org>
2009-12-06 01:15:50 +0000

    Modified field initialization to use Drupal.behaviors

--- js/button_field.js
+++ js/button_field.js
@@ -1,21 +1,17 @@
 // $Id$
-(function($) {
-  $.button_field = {
-    init: function(element) {
-      $('#'+element).bind('click', function() {
-        var $this = $(this),
-          success_url = $this.attr('data-button_field_url');
-        
-        $.ajax({
-          url: '/button_field/callback',
-          data: {id: $this.attr('id')},
-          success: function() {
-            if (success_url) {
-              location.href = success_url;
-            }
-          }
-        }); // end $.ajax()
-      }); // end function $.bind()
-    } // end function init
-  }; // end $.button_field
-})(jQuery);
+Drupal.behaviors.buttonFieldBehavior = function(context) {
+  $('.button_field').bind('click', function() {
+    var $this = $(this),
+      success_url = $this.attr('data-button_field_url');
+    
+    $.ajax({
+      url: '/button_field/callback',
+      data: {id: $this.attr('id')},
+      success: function() {
+        if (success_url) {
+          location.href = success_url;
+        }
+      }
+    }); // end $.ajax()
+  }); // end function $.bind()
+}; // end function buttonFieldBehavior()
