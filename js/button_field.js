1699f0a8bcc2209338a1ae7f5c75b2faffbf0c56
James I. Armes <bassistjimmyjam@284457.no-reply.drupal.org>
2010-06-06 01:56:52 +0000

    adding missing path to the data object

--- js/button_field.js
+++ js/button_field.js
@@ -4,7 +4,10 @@ Drupal.behaviors.buttonFieldBehavior = function(context) {
     $.ajax({
       url: Drupal.settings.basePath+'button_field/callback',
       dataType: 'json',
-      data: {id: $(this).attr('id')},
+      data: {
+         id: $(this).attr('id'),
+         path: location.pathname
+      },
       success: function(data) {
         if (data.redirect) {
           document.location.href = data.redirect;
