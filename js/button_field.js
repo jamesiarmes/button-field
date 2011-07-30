293cf8b99010a46b975de7b58d10cf00609e16fe
James I. Armes <jamesiarmes@gmail.com>
2011-07-30 18:42:48 -0400

    Issue #825516 by zeezhao: Added an option to show a confirmation message to the user before running rules

--- js/button_field.js
+++ js/button_field.js
@@ -1,18 +1,29 @@
 Drupal.behaviors.buttonFieldBehavior = function(context) {
   $('.button_field').bind('click', function() {
-    $.ajax({
-      url: Drupal.settings.basePath+'button_field/callback',
-      dataType: 'json',
-      data: {
-         id: $(this).attr('id'),
-         path: location.pathname
-      },
-      success: function(data) {
-        if (data.redirect) {
-          document.location.href = data.redirect;
+    var id = $(this).attr('id'),
+      process = true;
+    
+    // if a confirmation message was provided then display it now
+    if (Drupal.settings[id].confirmation) {
+      process = confirm(Drupal.settings[id].confirmation);
+    } // end if a confirmation message was provided
+    
+    // if we should process the rules for this button then do so now
+    if (process) {
+      $.ajax({
+        url: Drupal.settings.basePath+'button_field/callback',
+        dataType: 'json',
+        data: {
+           id: $(this).attr('id'),
+           path: location.pathname
+        },
+        success: function(data) {
+          if (data.redirect) {
+            document.location.href = data.redirect;
+          }
         }
-      }
-    }); // end $.ajax()
+      }); // end $.ajax()
+	} // end if we should process this button
     
     return false;
   }); // end function $.bind()
